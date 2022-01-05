034d5a190bf6f8e3f46df91289b8d817d3b554ff
Ibrahim Shittu <ibshittu01@gmail.com>
2022-01-05 20:56:12 +0100

    incomeexpensesapi

---
+++ incomexpensesapi/authentication/models.py
@@ -0,0 +1,62 @@
+from django.db import models
+from django.contrib.auth.models import (
+    BaseUserManager, AbstractBaseUser, PermissionsMixin)
+# Create your models here.
+
+
+class UserManager(BaseUserManager):
+    def create_user(self, username, email, password=None):
+        if not username:
+            raise ValueError('Users must have a username')
+        if not email:
+            raise ValueError('Users must have an email address')
+
+        user = self.model(
+            email=self.normalize_email(email),
+            username=username,
+        )
+        user.set_password(password)
+        user.save()
+        return user
+
+    def create_superuser(self, email, username, password=None):
+        """
+        Creates and saves a superuser with the given email, date of
+        birth and password.
+        """
+
+        if not password:
+            raise ValueError('Users must have a password')
+
+        user = self.create_user(
+            email,
+            password=password,
+            username=username,
+        )
+        user.is_admin = True
+        user.is_staff = True
+        user.save()
+        return user
+
+
+class User(AbstractBaseUser):
+    username = models.CharField(max_length=255, unique=True, db_index=True)
+    email = models.EmailField(
+        verbose_name='email address', max_length=255, unique=True)
+    is_verified = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+    is_admin = models.BooleanField(default=False)
+    is_staff = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    created_at = models.DateTimeField(auto_now=True)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username']
+
+    objects = UserManager()
+
+    def __str__(self):
+        return self.email
+
+    def token(self):
+        return ""
